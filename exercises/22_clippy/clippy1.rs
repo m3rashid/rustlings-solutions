14303f6bb51dfa56ca00a792cfe720a4af34a495
m3rashid <m3rashid.hussain@gmail.com>
2024-02-18 12:33:50 +0530

    iterators

--- exercises/22_clippy/clippy1.rs
+++ exercises/22_clippy/clippy1.rs
@@ -11,13 +11,13 @@
 
 // I AM NOT DONE
 
-use std::f32;
+use std::f32::{self, consts};
 
 fn main() {
-    let pi = 3.14f32;
+    // let pi = 3.14f32;
     let radius = 5.00f32;
 
-    let area = pi * f32::powi(radius, 2);
+    let area = consts::PI * f32::powi(radius, 2);
 
     println!(
         "The area of a circle with radius {:.2} is {:.5}!",
